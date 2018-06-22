e16983d215f6b20f39064e5cf1fc174d4960704e
Lan <lanpham312@gmail.com>
2018-06-22 14:55:52 +0100

    refactoring

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -6,25 +6,22 @@ defmodule RotationalCipher do
   iex> RotationalCipher.rotate("Attack at dawn", 13)
   "Nggnpx ng qnja"
   """
-  numbers = Enum.to_list(0..25)
-  alphabet = Enum.map(?a..?z, fn x -> <<x::utf8>> end)
-  @code Map.new(Enum.zip(alphabet, numbers))
+  numbers_list = Enum.to_list(0..25)
+  alphabet_list = Enum.map(?a..?z, fn x -> <<x::utf8>> end)
+  @code_map Map.new(Enum.zip(alphabet_list, numbers_list))
 
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
-    array_of_numbers =
       text
       |> String.codepoints()
-      |> Enum.map(fn x -> rem(@code[x] + shift, 26) end)
-
-    array_of_numbers
-    |> Enum.map(fn x -> converter(x) end)
-    |> List.to_string()
+      |> Enum.map(fn x -> rem(@code_map[x] + shift, 26) end)
+      |> Enum.map(fn x -> converter(x) end)
+      |> List.to_string()
   end
 
   def converter(new_number) do
-    @code
-    |> Enum.find(fn {letter, number} -> number == new_number end)
+    @code_map
+    |> Enum.find(fn {_, number} -> number == new_number end)
     |> elem(0)
   end
 end
